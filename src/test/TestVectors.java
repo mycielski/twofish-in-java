2265d105575754ce927a3debfa4749fa1a53a212
Mycielski <76878490+Mycielski@users.noreply.github.com>
2021-06-08 12:51:42 +0200

    added test vectors

--- src/test/TestVectors.java
+++ src/test/TestVectors.java
@@ -13,17 +13,117 @@ import twofish.exceptions.WrongNumberOfBitsException;
 class TestVectors {
 
     @Test
-    @DisplayName("KEYSIZE=128\n" +
-            "\n" +
-            "KEY=00000000000000000000000000000000\n" +
-            "PT=00000000000000000000000000000000\n")
     public void testVector1() throws Exception {
         String key = "00000000000000000000000000000000";
         String plaintext = "00000000000000000000000000000000";
-        byte[] ciphertext = Twofish.twofishECBEncrypt(plaintext, key);
-        assertArrayEquals(removePaddingFromCiphertext(ciphertext), hexStringToByteArray("9F589F5CF6122C32B6BFEC2F2AE8C35A"));
-        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertext, key), hexStringToByteArray(plaintext));
+        String ciphertext = "9F589F5CF6122C32B6BFEC2F2AE8C35A";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
     }
+
+    @Test
+    public void testVector2() throws Exception {
+        String key = "00000000000000000000000000000000";
+        String plaintext = "9F589F5CF6122C32B6BFEC2F2AE8C35A";
+        String ciphertext = "D491DB16E7B1C39E86CB086B789F5419";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+    @Test
+    public void testVector3() throws Exception {
+        String key = "9F589F5CF6122C32B6BFEC2F2AE8C35A";
+        String plaintext = "D491DB16E7B1C39E86CB086B789F5419";
+        String ciphertext = "019F9809DE1711858FAAC3A3BA20FBC3";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+    @Test
+    public void testVector4() throws Exception {
+        String key = "000000000000000000000000000000000000000000000000";
+        String plaintext = "00000000000000000000000000000000";
+        String ciphertext = "EFA71F788965BD4453F860178FC19101";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+    @Test
+    public void testVector5() throws Exception {
+        String key = "000000000000000000000000000000000000000000000000";
+        String plaintext = "EFA71F788965BD4453F860178FC19101";
+        String ciphertext = "88B2B2706B105E36B446BB6D731A1E88";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+    @Test
+    public void testVector6() throws Exception {
+        String key = "EFA71F788965BD4453F860178FC191010000000000000000";
+        String plaintext = "88B2B2706B105E36B446BB6D731A1E88";
+        String ciphertext = "39DA69D6BA4997D585B6DC073CA341B2";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+    @Test
+    public void testVector7() throws Exception {
+        String key = "88B2B2706B105E36B446BB6D731A1E88EFA71F788965BD44";
+        String plaintext = "39DA69D6BA4997D585B6DC073CA341B2";
+        String ciphertext = "182B02D81497EA45F9DAACDC29193A65";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+    @Test
+    public void testVector8() throws Exception {
+        String key = "0000000000000000000000000000000000000000000000000000000000000000";
+        String plaintext = "00000000000000000000000000000000";
+        String ciphertext = "57FF739D4DC92C1BD7FC01700CC8216F";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+    @Test
+    public void testVector9() throws Exception {
+        String key = "0000000000000000000000000000000000000000000000000000000000000000";
+        String plaintext = "57FF739D4DC92C1BD7FC01700CC8216F";
+        String ciphertext = "D43BB7556EA32E46F2A282B7D45B4E0D";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+    @Test
+    public void testVector10() throws Exception {
+        String key = "57FF739D4DC92C1BD7FC01700CC8216F00000000000000000000000000000000";
+        String plaintext = "D43BB7556EA32E46F2A282B7D45B4E0D";
+        String ciphertext = "90AFE91BB288544F2C32DC239B2635E6";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+
+    @Test
+    public void testVector11() throws Exception {
+        String key = "D43BB7556EA32E46F2A282B7D45B4E0D57FF739D4DC92C1BD7FC01700CC8216F";
+        String plaintext = "90AFE91BB288544F2C32DC239B2635E6";
+        String ciphertext = "6CB4561C40BF0A9705931CB6D408E7FA";
+        byte[] ciphertextBytes = Twofish.twofishECBEncrypt(plaintext, key);
+        assertArrayEquals(removePaddingFromCiphertext(ciphertextBytes), hexStringToByteArray(ciphertext));
+        assertArrayEquals(Twofish.twofishECBDecrypt(ciphertextBytes, key), hexStringToByteArray(plaintext));
+    }
+
+
 
     @Test
     @DisplayName("Wrong key length")
